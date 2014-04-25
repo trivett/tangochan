59a6e6ee45826d9e460f53ad682acd29511e5796
trivett <trivett@gmail.com>
2014-04-25 16:03:50 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tangochan::Application.config.secret_key_base = 'fced8e80ddfe9ad22f2c1448922aa1e38e87e0fd10731aa2c32e52ecd5f7796ae9afa8d256e6e3f4d61e71fbfb345383391a08acd14a2b20d8c470f04163d64f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tangochan::Application.config.session_store :cookie_store, key: '_tangochan_session'
