b56f978669ffb3b31cb97961a056732951e46838
trivett <trivett@gmail.com>
2014-04-26 11:19:25 -0400

    added basic attributes to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140426145824) do
+ActiveRecord::Schema.define(version: 20140426151717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20140426145824) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "name"
+    t.string   "native_language"
+    t.string   "learning_language"
+    t.string   "city"
+    t.string   "country"
+    t.integer  "words_practiced"
+    t.integer  "minutes_practiced"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
 Word.create(kanji: "一", kana: "いち", english: "one")
 Word.create(kanji: "二", kana: "に", english: "two")
 Word.create(kanji: "三", kana: "さん", english: "three")
