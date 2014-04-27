7a4244ddc5ba09d0014d2b40769b5fabfa993db6
trivett <trivett@gmail.com>
2014-04-27 12:50:05 -0400

    trying to render JS in lists#edit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,6 @@
 class ListsController < ApplicationController
 
-  def index
-    @lists = List.where(:user_id => current_user.id)
-  end
+
 
   def new
     @user = current_user
@@ -14,23 +12,12 @@ class ListsController < ApplicationController
     @user = current_user
     @list = List.create(title: params[:list][:title], user_id: @user.id)
     redirect_to edit_list_path(@list)
-
-
-    #  @hoot = Hoot.create(content: params[:hoot][:content], author: params[:hoot][:author])
-    # # Redirect to somewhere
-    # respond_to do |format|
-    #   format.html { redirect_to '/' }
-    #   format.js   { }
-    #   format.json { render json: hoot.to_json }
-    # end
-
   end
 
   def edit
     @list = List.find(params[:id])
     respond_to do |format|
-      format.js   { }
-      format.json { render json: list.to_json }
+      format.js { render js  }
     end
   end
 
