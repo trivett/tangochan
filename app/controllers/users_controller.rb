82b508bf74d7999abf155e8b94cf98a51ea313ad
trivett <trivett@gmail.com>
2014-04-27 13:57:33 -0400

    create list now comes in a sweet sweet modal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     if current_user
       @user = current_user
       @lists = List.where(:user_id => current_user.id)
+      @list = List.new
     else
       redirect_to root_path
     end
