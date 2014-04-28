2ce00f313ad1ce4238e51a12c92a29cde6dc383c
trivett <trivett@gmail.com>
2014-04-28 10:33:36 -0400

    switching to 28

--- app/assets/javascripts/list.js.coffee
+++ app/assets/javascripts/list.js.coffee
@@ -1,4 +1,2 @@
-console.log("loaded brooooo");
-
 
 
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,9 +2,20 @@ class WordsController < ApplicationController
 
   def search
     term = params[:term]
+    list = params[:list]
     @results = Word.where("kanji like '%#{term}%'")
+    @list = List.find(list)
     respond_to do |format|
       format.js {}
     end
   end
+
+
+  def add_to_list
+    word_to_add = params[:word]
+
+
+
+  end
+
 end
