59177c879e24a47c6f64b6c6941d7b124665c6f5
dan987 <dan987@aol.com>
2014-05-01 09:24:38 -0400

    Edit user fix.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id", :uniq => true
   has_many :inverse_friends, through: :inverse_friendships, :source => :user,  :uniq => true
 
-  validates :name, :email, :password, :password_confirmation,  presence: true
+  validates :name, :email,  presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
