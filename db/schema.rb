73fec00c4ca7e7573ca12b161c256e2630757a03
jcohenca <jcohenca@gmail.com>
2014-05-01 14:25:28 -0400

    added user_name to lists table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429130853) do
+ActiveRecord::Schema.define(version: 20140501181945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 20140429130853) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "user_name"
+  end
+
+  create_table "lists_users", force: true do |t|
+    t.integer "list_id"
+    t.integer "user_id"
   end
 
   create_table "lists_words", force: true do |t|
